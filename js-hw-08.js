cc484d9d8e49baa0067281574543e460e3b12614
Valentina Perishko <valentina.perishko@gmail.com>
2020-11-28 21:48:00 -0600

    adds changes

--- js-hw-08.js
+++ js-hw-08.js
@@ -1,9 +1,11 @@
 import images from "./gallery-items.js";
 
 const refs = {
-  galleryList: document.querySelector('ul.gallery'),
-  lightbox: document.querySelector('.lightbox'),
-  btn: document.querySelector('[data-action="close-lightbox"]')
+  galleryList: document.querySelector('.js-gallery'),
+  lightbox: document.querySelector('.js-lightbox'),
+  btn: document.querySelector('[data-action="close-lightbox"]'),
+  overlay: document.querySelector('.lightbox__overlay'),
+  imageLightbox: document.querySelector('.lightbox__image')
 };
 
 const createImage = (item, parent) => {
@@ -16,7 +18,8 @@ const createImage = (item, parent) => {
     img.alt = description;
     
     parent.appendChild(img);
-  };
+};
+  
   
   const createLink = (item, parent) => {
     const { original } = item;
@@ -26,7 +29,7 @@ const createImage = (item, parent) => {
     a.href = original;
     
     createImage(item, a);
-    
+        
     parent.appendChild(a);
   };
   
@@ -40,31 +43,76 @@ const createImage = (item, parent) => {
   };
   
   const renderListItems = (arr) => {
-    const items = arr.map( (item) => createItem(item));
+    const items = arr.map((item) => createItem(item));
     
     refs.galleryList.append(...items);
   };
-  
   renderListItems(images);
-  
-  
   
   
   function onClickHandler(e) {
     e.preventDefault();
+    refs.lightbox.classList.add('is-open');
+    refs.imageLightbox.src = e.target.dataset.source;
+    refs.imageLightbox.alt = e.target.alt;
+    refs.imageLightbox.dataset.index = e.target.dataset.index;
     
-    if(e.target.nodeName === 'IMG') {
-      refs.lightbox.classList.add('is-open');
-      refs.lightbox.querySelector('.lightbox__image').src = e.target.src;
-      refs.lightbox.querySelector('.lightbox__image').alt = e.target.alt;
-    }
-  }
+  };
   
-  function onCloseHandler(e) {
-    if(e.target.nodeName === "I" || e.target.nodeName === "BUTTON") {
-      refs.lightbox.classList.remove('is-open');
-    }
-  }
-  
+  function onCloseHandler() {
+    refs.lightbox.classList.remove('is-open');
+    refs.imageLightbox.src = "";
+    refs.imageLightbox.alt = "";
+    
+  };
+
   refs.galleryList.addEventListener('click', onClickHandler);
   refs.btn.addEventListener('click', onCloseHandler);
+  refs.overlay.addEventListener('click', onCloseHandler);
+  
+  window.addEventListener("keydown", (event) => {
+    if(event.key === "Escape") {
+      onCloseHandler()
+    }
+    if(event.key === "ArrowLeft") {
+      arrowLeft()
+    }
+    if(event.key === "ArrowRight") {
+      arrowRight()
+    }
+  });
+
+  function setLightboxImageAttribute(step, index) {
+    refs.imageLightbox.dataset.index = `${index + step}`;
+    refs.imageLightbox.src = images[index + step].original
+  };
+
+  function arrowLeft() {
+    let index = +refs.imageLightbox.dataset.index;
+    if(index === 0) {
+      setLightboxImageAttribute(0, images.length - 1)
+      return
+    }
+    console.log(index);
+    setLightboxImageAttribute(-1, index);
+  };
+
+  function arrowRight() {
+    let index = +refs.imageLightbox.dataset.index;
+    if(index === images.length - 1) {
+      setLightboxImageAttribute(0, 0)
+      return
+    }
+    console.log(index);
+    setLightboxImageAttribute(1, index);
+  }
+
+  
+  
+
+
+  
+
+  
+
+  
